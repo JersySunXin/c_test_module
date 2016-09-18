0f0ec30bd23b1f802a9d2036c6084f2d99329267
sunxin <sun01090109@163.com>
2016-09-18 14:36:42 +0800

    add a c prj

---
+++ fileproc01/main.c
@@ -0,0 +1,35 @@
+/*
+    file proc test
+
+*/
+#include "stdio.h"
+#include "stdlib.h"
+//#include <>
+
+int main()
+{
+    FILE *fp;
+    printf("proc is start!\n");
+
+    //open and make
+    if((fp=fopen("test","w"))==NULL)
+    {
+        printf("File cannot opened!\n");
+    }
+    else
+    {
+        printf("File opened for writing!\n");
+    }
+
+    //close
+    if(fclose(fp)!=0)
+    {
+        printf("File cannot close!\n");
+    }
+    else
+    {
+        printf("File is closed!\n");
+    }
+    getchar();
+    return 0;
+}
